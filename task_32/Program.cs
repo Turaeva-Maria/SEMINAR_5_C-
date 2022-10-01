45d51cadb8bd07e9f7f95170e9e8bed0fc681c9a
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-01 18:56:15 +0300

    Программа замены положительных элементов массива на отрицательные

---
+++ task_32/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 32: Напишите программу замена элементов массива: положительные элементы замените 
+на соответствующие отрицательные, [-4, -8, 8, 2] -> [4, 8, -8, -2]*/
+
+Console.Clear();
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = array[i] * (-1);
+
+}
+
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+ReleaseArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
