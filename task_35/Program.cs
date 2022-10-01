06ad4d13f9870240b8e5057390c50d9f415a14ab
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-01 19:12:24 +0300

    Программа на нахождение количества элементов массива в отрезке от 10 до 99

---
+++ task_35/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5*/
+
+Console.Clear();
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 201);
+}
+
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+            count++;
+    }
+    return count;
+}
+
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine("Количество элементов массива в интервале от 10 до 99: ");
+Console.WriteLine(ReleaseArray(array));
+
+
