0eb55954a48d7b076013dff3bd525e3d781f8186
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-01 16:12:28 +0300

    Программа на создание массива из трехзначных чисел и поиска количества четных чисел в нем

---
+++ task_34/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 34: Задайте массив, заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+Console.Clear();
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int AccountNum (int[] array)
+{
+    int count = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        if (array[i] % 2 == 0)
+
+        {
+        count++;
+        }     
+
+    }
+
+    return count;
+}
+
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine(AccountNum(array));
+
