0cc45decd43a04c5fe950d7a26d360cdee702bf4
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-01 19:05:52 +0300

    Программа на определение - есть ли заданное число в массиве

---
+++ task_33/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 33: Задайте массив. Напишите программу, которая определяет, 
+присутствует ли заданное число в массиве. 
+ 4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да*/
+
+Console.Clear();
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+
+string ReleaseArray(int[] array, int x)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == x)
+            return "Да, это число есть в массиве";
+    }
+    return "Нет, такого числа нет в массиве";
+}
+
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.Write("Введите число для проверки его нахождения в массиве: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ReleaseArray(array, x));
